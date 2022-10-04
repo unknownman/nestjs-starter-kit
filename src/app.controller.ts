7b9b2709da46db87a149b14fde5d45b8dba2cedc
ali alavi <ali.masoudi.alavi@gmail.com>
2022-10-04 09:47:54 +0330

    mysql and redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /*
+   * implementing cache
+   */
+  @UseInterceptors(CacheInterceptor) // automatic cache active
+  @CacheKey('custom-key') // define custum cache key name
+  @CacheTTL(30) // override TTL from 5 seconds(default) to 30 seconds
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+      isGlobal: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
   ],
   controllers: [AppController],
